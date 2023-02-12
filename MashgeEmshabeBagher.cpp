9849fb6564aa546156801fb9d76758f58ec23025
Mohammad <86884461+MohammadBidkhori@users.noreply.github.com>
2023-02-12 13:21:18 +0330

    Create MashgeEmshabeBagher.cpp

---
+++ MashgeEmshabeBagher.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str,temp;
+    int sum=0;
+    getline(cin,str);
+    for(int i=0,j=0;j<3;i++){
+        if(str[i]!=' '){
+            temp+=str[i];
+        }
+        else{
+            if(stoi(temp)==0 || stoi(temp)>=180){
+                break;
+            }else{
+                sum+=stoi(temp);
+                temp="";
+                j++;
+            }
+        }
+    }
+    if(sum == 180){
+        cout << "Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
